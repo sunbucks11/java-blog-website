d79478084ca003a6dd2964044f42e8512daa0161
Semir Ibrahim <semir@teamnetsol.com>
2015-09-22 17:38:45 +0100

    Added verification.jsp file and VerificationControllerfile.java file.

---
+++ src/main/java/com/java/blog/controller/VerificationController.java
@@ -0,0 +1,13 @@
+package com.java.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VerificationController {
+  @RequestMapping("/verification")
+  public String verification() {
+    return "verification";
+  }
+
+}
